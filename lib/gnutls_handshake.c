57014348eef621ec51a76acc28fcd270edbd0235
Jonathan Bastien-Filiatrault <joe@x2a.org>
2009-08-03 13:35:57 -0400

    Move _gnutls_handshake_hash_init next to _gnutls_handshake_hash_buffers_clear.

--- lib/gnutls_handshake.c
+++ lib/gnutls_handshake.c
@@ -66,6 +66,40 @@ int _gnutls_server_select_comp_method (gnutls_session_t session,
 				       opaque * data, int datalen);
 
 
+/* This function initialized the handshake hash session.
+ * required for finished messages.
+ */
+inline static int
+_gnutls_handshake_hash_init (gnutls_session_t session)
+{
+
+  if (session->internals.handshake_mac_handle_init == 0)
+    {
+      int ret =
+	_gnutls_hash_init (&session->internals.handshake_mac_handle_md5,
+			   GNUTLS_MAC_MD5);
+
+      if (ret < 0)
+	{
+	  gnutls_assert ();
+	  return ret;
+	}
+
+      ret =
+	_gnutls_hash_init (&session->internals.handshake_mac_handle_sha,
+			   GNUTLS_MAC_SHA1);
+      if (ret < 0)
+	{
+	  gnutls_assert ();
+	  return GNUTLS_E_MEMORY_ERROR;
+	}
+
+      session->internals.handshake_mac_handle_init = 1;
+    }
+
+  return 0;
+}
+
 /* Clears the handshake hash buffers and handles.
  */
 static void
@@ -2181,40 +2215,6 @@ _gnutls_abort_handshake (gnutls_session_t session, int ret)
   return GNUTLS_E_INTERNAL_ERROR;
 }
 
-
-/* This function initialized the handshake hash session.
- * required for finished messages.
- */
-inline static int
-_gnutls_handshake_hash_init (gnutls_session_t session)
-{
-
-  if (session->internals.handshake_mac_handle_init == 0)
-    {
-      int ret =
-	_gnutls_hash_init (&session->internals.handshake_mac_handle_md5,
-			   GNUTLS_MAC_MD5);
-
-      if (ret < 0)
-	{
-	  gnutls_assert ();
-	  return ret;
-	}
-
-      ret =
-	_gnutls_hash_init (&session->internals.handshake_mac_handle_sha,
-			   GNUTLS_MAC_SHA1);
-      if (ret < 0)
-	{
-	  gnutls_assert ();
-	  return GNUTLS_E_MEMORY_ERROR;
-	}
-
-      session->internals.handshake_mac_handle_init = 1;
-    }
-
-  return 0;
-}
 
 static int
 _gnutls_send_supplemental (gnutls_session_t session, int again)
